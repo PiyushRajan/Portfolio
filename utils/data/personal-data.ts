5f18e8d3d1559d166a6536aec6fa8864ed4186e4
PiyushRajan <110221132+PiyushRajan@users.noreply.github.com>
2026-02-20 17:09:34 +0530

    Update personal-data.ts

--- utils/data/personal-data.ts
+++ utils/data/personal-data.ts
@@ -4,7 +4,7 @@ export const personalData = {
   name: "Piyush Rajan",
   profile: '/my.jpg',
   designation: "Software Developer",
-  description: "Hi, I’m Piyush Rajan — a dedicated Software Developer from Mohali, Punjab. I completed my Bachelor’s in Computer Applications from Panjab University, Chandigarh, one of the most reputed institutions in the region. I am passionate about building clean, efficient, and user‑focused software solutions. With a consistent and organized working style, I have a strong track record of meeting project and company goals. I enjoy solving real-world problems through technology and continuously improving my skills as a developer.",
+  description: "Hi, I’m Piyush Rajan — a Software Developer based in Mohali, Punjab. I graduated with a Bachelor’s in Computer Applications from Panjab University, Chandigarh.I specialize in building scalable, high-performance web applications using modern technologies like React.js and Next.js. I enjoy designing reusable component architectures, developing role-based systems, and creating intuitive user experiences.I focus on writing clean, maintainable code and delivering production-ready solutions that solve real-world business problems. I’m constantly learning and refining my skills to stay aligned with evolving web technologies.",
   email: 'piyushrajan05@gmail.com',
   phone: '7087632814',
   address: 'Mohali',
